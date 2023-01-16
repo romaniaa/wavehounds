c9950e9f3323ec96386be38d3739cad8c209b291
roman-overhaul <roman@overhaulmedia.com>
2023-01-15 21:44:49 -0800

    adding env variables

--- src/pages/work.js
+++ src/pages/work.js
@@ -3,10 +3,10 @@ import Layout from '../components/Layout'
 import { workImage } from "../staticContent"
 import InstaFeeds from '../components/InstaFeeds'
 
-
 const WorkPage = () => {
   return (
     <Layout pageTitle="My Work Posts" sideImage={workImage ? workImage : ''}>
+      {console.log(process.env.GATSBY_INS_TOKEN)}
       <p>A lot of important work happens at Wavehounds, below is some of the other stuff. </p>
       <div>
         <InstaFeeds token={process.env.GATSBY_INS_TOKEN} limit={12}/>
