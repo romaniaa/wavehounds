3e081ddfa5522a91437187e9b554c067072b07d9
roman-overhaul <roman@overhaulmedia.com>
2023-01-15 20:03:40 -0800

    updating import

--- src/pages/work.js
+++ src/pages/work.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import * as React from 'react'
 import Layout from '../components/Layout'
 import { workImage } from "../staticContent"
 import InstaFeeds from '../components/InstaFeeds'
